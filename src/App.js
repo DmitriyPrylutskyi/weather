b4591aaa08d6180067434ff4e0c96e6bdeaac5fd
dmitriyprylutskyi <dprily@gmail.com>
2017-10-25 10:43:20 +0300

    Add npm deploy, https, change style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 //import "bootstrap/dist/css/bootstrap.css";
-import "bootswatch/united/bootstrap.css";
+import "bootswatch/cerulean/bootstrap.css";
 import { Navbar, NavItem, Nav, Grid, Row, Col } from "react-bootstrap";
 import './App.css';
 
@@ -44,7 +44,7 @@ class App extends Component {
                                 }}
                             >
                                 {PLACES.map((place, index) => (
-                                    <NavItem key={index} eventKey={index}>{place.name}</NavItem>
+                                    <NavItem key={index} eventKey={index} className="Nav-item">{place.name}</NavItem>
                                 ))}
                             </Nav>
                         </Col>
@@ -69,10 +69,10 @@ class WeatherDisplay extends Component {
 
     componentDidMount() {
         const id = this.props.id;
-        const URL = "http://api.openweathermap.org/data/2.5/weather?id=" +
+        const URL = "https://api.openweathermap.org/data/2.5/weather?id=" +
             id +
             "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=metric";
-        const URLforecast = "http://api.openweathermap.org/data/2.5/forecast/daily?id=" +
+        const URLforecast = "https://api.openweathermap.org/data/2.5/forecast/daily?id=" +
             id +
             "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=metric&cnt=5";
         fetch(URL).then(res => res.json()).then(json => {
@@ -88,7 +88,7 @@ class WeatherDisplay extends Component {
         const weatherDataForecast = this.state.weatherDataForecast;
         if (!weatherData) return <div>Loading...</div>;
         const weather = weatherData.weather[0];
-        const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
+        const iconUrl = "https://openweathermap.org/img/w/" + weather.icon + ".png";
         if (!weatherDataForecast) return <div>Loading...</div>;
         const weatherForecast = weatherDataForecast.list;
         const currentTemp = Math.round(weatherData.main.temp);
@@ -96,16 +96,18 @@ class WeatherDisplay extends Component {
         let dates = [];
         for (let i = 0; i < 5; i++) {
             let date = new Date(weatherForecast[i].dt*1000).toDateString();
-            let iconForecast = "http://openweathermap.org/img/w/" + weatherForecast[i].weather[0].icon + ".png";
+            let iconForecast = "https://openweathermap.org/img/w/" + weatherForecast[i].weather[0].icon + ".png";
             let dayTemp = Math.round(weatherForecast[i].temp.day);
             let nightTemp = Math.round(weatherForecast[i].temp.night);
             let wind_speed = Math.round(weatherForecast[i].speed);
             dates.push(
-                <h3>Date: {date}</h3>,
-                <p>{weatherForecast[i].weather[0].main} <img src={iconForecast} alt={weatherForecast[i].weather.description} /></p>,
-                <p>Temperature Day: {dayTemp}°&nbsp;</p>,
-                <p>Temperature Night: {nightTemp}°&nbsp;</p>,
-                <p>Wind Speed: {wind_speed} m/s</p>
+                <div key={i}>
+                    <h3>Date: {date}</h3>
+                    <p>{weatherForecast[i].weather[0].main} <img src={iconForecast} alt={weatherForecast[i].weather.description} /></p>
+                    <p>Temperature Day: {dayTemp}°</p>
+                    <p>Temperature Night: {nightTemp}°</p>
+                    <p>Wind Speed: {wind_speed} m/s</p>
+                </div>
             );
         }
         return (
@@ -114,7 +116,7 @@ class WeatherDisplay extends Component {
                     {weather.main} in {weatherData.name}
                     <img src={iconUrl} alt={weatherData.description} />
                 </h1>
-                <p>Current Temperature: {currentTemp}°&nbsp;</p>
+                <p>Current Temperature: {currentTemp}°</p>
                 <p>Wind Speed: {currentWindSpeed} m/s</p>
                 <h2>Forecast on 5 days</h2>
                 {dates}
